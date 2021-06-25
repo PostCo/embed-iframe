64377855afcc9016057bda70105556a6befa73eb
Er Whey <erwhey@gmail.com>
2021-06-25 22:55:10 +0800

    mend

--- iframe-listener.js
+++ iframe-listener.js
@@ -1,16 +1,20 @@
-document.addEventListener("DOMContentLoaded", function() {
+  document.addEventListener("DOMContentLoaded", function() {
     const iframe = document.querySelector('#postco360-iframe');
     let initialHeight;
 	window.onmessage = (event) => {
       if (event.origin.match(/[http[s]?:\/\/360\.postco\.co\/|ngrok/))
       {	
+        console.log(event.data)
         let { height, resetStyle } = event.data;
+        
         if(!initialHeight) initialHeight = height
+        
         if(resetStyle) {
-          iframe.scrollIntoView()
           height = initialHeight
         }
+        
         iframe.style.height = height + 'px';
+        iframe.scrollIntoView()
       }
-  }
-}); 
+    }
+  });
