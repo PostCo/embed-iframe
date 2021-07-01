ea15cef3ac84b07deabc6f260902d21d6a85f4b5
Er Whey <erwhey@gmail.com>
2021-07-01 16:40:32 +0800

    update listener code

--- iframe-listener.js
+++ iframe-listener.js
@@ -1,20 +1,20 @@
-  document.addEventListener("DOMContentLoaded", function() {
-    const iframe = document.querySelector('#postco360-iframe');
-    let initialHeight;
-	window.onmessage = (event) => {
+document.addEventListener("DOMContentLoaded", function() {
+  const iframe = document.querySelector('#postco360-iframe');
+  let initialHeight;
+  let padding = 50
+  window.onmessage = (event) => {
       if (event.origin.match(/[http[s]?:\/\/360\.postco\.co\/|ngrok/))
       {	
-        console.log(event.data)
+        // console.log('Parent received---', event.data)
         let { height, resetStyle } = event.data;
-        
+        height += padding  
         if(!initialHeight) initialHeight = height
         
         if(resetStyle) {
           height = initialHeight
-        }
-        
+        } 
         iframe.style.height = height + 'px';
-        iframe.scrollIntoView()
+        iframe.scrollIntoView(true)
       }
     }
-  });
+});
