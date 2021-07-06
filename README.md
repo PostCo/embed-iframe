29c83de374fd7ed9d0c80fac2c3527689c80fcd7
Er Whey <erwhey@gmail.com>
2021-07-06 17:04:58 +0800

    update

--- README.md
+++ README.md
@@ -3,9 +3,9 @@ Files to place on customers page with iframe pointed to their Returns page. It l
 
 ### How it works
 The parent window contains this JS code for listening to postMessages {object} send from within a child iframe.
-1. First default iframe height set and stored by the parent: 80vh.
+1. First default iframe height set and stored by the parent.
 2. Parent listens for a height param from child and changes height of its iframe.
-3. Parent listens for a resetStyle param from child and will reset iframe height to the stored height 80vh (this is used when pages have changed i.e. Home > Success)
+3. Parent listens for a resetStyle param from child and will reset iframe height to the stored height (this is used when pages have changed i.e. Home > Success)
 
 ### Sample Shopify Template Usage
 ```liquid
@@ -16,6 +16,5 @@ The parent window contains this JS code for listening to postMessages {object} s
 
 <!-- Files from this repo -->
 <script type="text/javascript" src="https://cdn.jsdelivr.net/gh/PostCo/embed-iframe/iframe-listener.min.js"></script>
-<link rel="stylesheet" href="https://cdn.jsdelivr.net/gh/PostCo/embed-iframe/style.min.css" />
 ```
 ### 
--- iframe-listener.js
+++ iframe-listener.js
@@ -1,20 +1,29 @@
 document.addEventListener("DOMContentLoaded", function() {
   const iframe = document.querySelector('#postco360-iframe');
-  let initialHeight;
-  let padding = 50
-  window.onmessage = (event) => {
-      if (event.origin.match(/[http[s]?:\/\/360\.postco\.co\/|ngrok/))
-      {	
-        // console.log('Parent received---', event.data)
-        let { height, resetStyle } = event.data;
-        height += padding  
-        if(!initialHeight) initialHeight = height
-        
-        if(resetStyle) {
-          height = initialHeight
-        } 
+  if (!iframe) return;
+  let shouldResize = true
+  let initialHeight = iframe.offsetHeight
+  let padding = 50;
+  console.log('Initial height > ', initialHeight)
+  
+window.onmessage = (event) => {
+    if (event.origin.match(/[http[s]?:\/\/360\.postco\.co\/|ngrok/))
+    {	
+      console.log('Parent received---', event.data)
+      
+      let { height, isRoot, resetHeight } = event.data;
+      height += padding
+      
+      // if the iframe is so much taller than the sources reported height, reset to the initial iframe height
+      if(shouldResize && isRoot && (iframe.offsetHeight - height) > 0 && (iframe.offsetHeight - height) / iframe.offsetHeight > 0.3){
+        iframe.style.height = `${initialHeight}px`;
+      } else if (resetHeight) {
+        shouldResize = false
+        iframe.style.height = `${initialHeight}px`
+      } else if (shouldResize && height > iframe.offsetHeight) {
         iframe.style.height = height + 'px';
         iframe.scrollIntoView(true)
-      }
+      }  
     }
+  }
 });
