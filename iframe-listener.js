1e6a5687c28477cb13637d477dece98604a751fa
Er Whey <erwhey@gmail.com>
2022-06-29 12:09:52 +0800

    add disable flag for debugging purposes

--- iframe-listener.js
+++ iframe-listener.js
@@ -1,19 +1,21 @@
 document.addEventListener("DOMContentLoaded", function() {
   const iframe = document.querySelector('#postco360-iframe');
-  if (!iframe) return;
+  const params = new URLSearchParams(window.location.search);
+
+  if (params.has('disable-resize-observer') || (!iframe)) return;
+
   let shouldResize = true
   let initialHeight = iframe.offsetHeight
   let padding = 50;
   console.log('Initial height > ', initialHeight)
-  
-window.onmessage = (event) => {
-    if (event.origin.match(/360\.postco\.co/))
-    {	
+
+  window.onmessage = (event) => {
+    if (event.origin.match(/360\.postco\.co/)) {
       console.log('Parent received---', event.data)
-      
+
       let { height, isRoot, resetHeight } = event.data;
       height += padding
-      
+
       // if the iframe is so much taller than the sources reported height, reset to the initial iframe height
       if(shouldResize && isRoot && (iframe.offsetHeight - height) > 0 && (iframe.offsetHeight - height) / iframe.offsetHeight > 0.3){
         iframe.style.height = `${initialHeight}px`;
@@ -23,7 +25,7 @@ window.onmessage = (event) => {
       } else if (shouldResize && height > iframe.offsetHeight) {
         iframe.style.height = height + 'px';
         iframe.scrollIntoView(true)
-      }  
+      }
     }
   }
 });
